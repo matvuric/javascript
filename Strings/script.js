b3fba47a8dae35fd34ea12427023c014cf24556a
matvuric <matviivk90@gmail.com>
2022-09-16 12:55:35 +0300

    sec

---
+++ Strings/script.js
@@ -0,0 +1,9 @@
+describe("ucFirst", function() {
+    it('Переводит первый символ в верхний регистр', function() {
+      assert.strictEqual(ucFirst("john"), "John");
+    });
+  
+    it("Не умирает на пустых строках", function() {
+      assert.strictEqual(ucFirst(""), "");
+    });
+  });
